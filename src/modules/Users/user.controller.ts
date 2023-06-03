51b4739468299788058220c1ba03ec2e04578be3
Ashim Rudra Paul <codewithashim@gmail.com>
2023-06-04 03:32:24 +0600

    hasky test

--- src/modules/Users/user.controller.ts
+++ src/modules/Users/user.controller.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
 import { createUserInDB } from './user.service'
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
+const createUser = async (reqx: Request, res: Response, next: NextFunction) => {
   try {
-                       const { user } = req.body
+    const { user } = req.body
     const result = await createUserInDB(user)
     res.status(201).json({
       success: true,
