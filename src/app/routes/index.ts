bfdf6993d39ab7efcb862062d9ef850e91ad8564
Ashim Rudra Paul <codewithashim@gmail.com>
2023-06-24 23:00:06 +0600

    [DONE] Faculty and dipertment

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { AcademicDepartmentRoutes } from '../modules/academicDipertment/academicDipertment.route';
+import { AcademicFacultyRoutes } from '../modules/academicFculty/academicFculty.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 import { UserRoutes } from '../modules/user/user.route';
 
@@ -7,11 +9,19 @@ const router = express.Router();
 const moduleRoutes = [
   {
     path: '/users',
-    route: UserRoutes,
+    route: UserRoutes, 
   },
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: AcademicDepartmentRoutes,
   },
 ];
 
